0c32624aa3bba6e89867f0f4e81fbe0c215e5f36
Zach Wegrzyniak <zach@streak.com>
2021-11-18 15:25:22 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var webpack_manifest_plugin_1 = require("webpack-manifest-plugin");
+var webpack_1 = require("webpack");
+console.log(webpack_manifest_plugin_1.WebpackManifestPlugin, webpack_1["default"]);
---
+++ webpack.config.ts
@@ -0,0 +1,4 @@
+import { WebpackManifestPlugin as ManifestPlugin } from "webpack-manifest-plugin";
+import webpack from "webpack";
+
+console.log(ManifestPlugin, webpack);
